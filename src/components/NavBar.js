510f5535e8df98494d116cbbf8834411d281b118
luorixo <luorixo@gmail.com>
2022-01-11 16:55:43 +1300

    added navbar styling and installed react social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 
 export default function NavBar() {
@@ -10,16 +11,21 @@ export default function NavBar() {
                     <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Eugene Chua
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink to="/post" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"> 
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink to="/project" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about" activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         About Me
                     </NavLink>
                 </nav>
+                <div>
+                    <SocialIcon />
+                    <SocialIcon />
+                    <SocialIcon />
+                </div>
             </div>
         </header>
     );
