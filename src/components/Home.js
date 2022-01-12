8054bf56fbfa9a3caa2fc0d6a51bd2a64d7e34ee
luorixo <luorixo@gmail.com>
2022-01-12 13:46:01 +1300

    added/changed fonts and styled social icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
 import NavBar from "./NavBar";
-import image from "../nzbush.jpg";
+import image from "../botanicplants.jpg";
 
 export default function Home() {
     return (
         <main>
             
-            <img src={image} alt="Ice Patterns" className="absolute z-0 object-cover w-full h-full"/>
+            <img src={image} alt="Botanic Garden Plants" className="absolute z-0 object-cover w-full h-full"/>
             <NavBar/>
-            <section className="relative flex justify-center pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-orange-100 font-bold cursive leading-none lg:leading-snug home-name">Hello! I'm Eugene.</h1>
+            <section className="relative flex justify-center pt-0 lg:pt-32 md:pt-2 px-8">
+                <h1 className="text-6xl text-orange-100 font-bold cursive leading-none lg:leading-snug home-name">Hello!</h1>
             </section>
         </main>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,17 @@ export default function NavBar() {
             <div className="container mx-auto flex justify-between">
                 <nav className="inline-flex">
                     <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-5 text-white text-4xl font-bold cursive tracking-widest">
-                        Eugene Chua
+                        Eugene C
                     </NavLink>
                     <NavLink to="/post" activeClassName="text-white bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-orange-400"> 
-                        Blog Posts
+                        Blog
                     </NavLink>
-                    <NavLink to="/project" activeClassName="text-white bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-green-800">
-                        Projects
-                    </NavLink>
+                   
                 </nav>
                 <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon url="https://www.linkedin.com/in/eugene-j-chua" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} />
-                    <SocialIcon url="https://github.com/luorixo" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} />
-                    <SocialIcon url="mailto:luorixo@gmail.com" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} />
+                    <SocialIcon url="https://www.linkedin.com/in/eugene-j-chua" className="mr-4" target="_blank" bgColor="#fff" style={{ height:40, width: 40}} />
+                    <SocialIcon url="https://github.com/luorixo" className="mr-4" target="_blank" bgColor="#fff" style={{ height:40, width: 40}} />
+                    <SocialIcon url="mailto:luorixo@gmail.com" className="" target="_blank" bgColor="#fff" style={{ height:40, width: 40}} />
                 </div>
             </div>
         </header>
