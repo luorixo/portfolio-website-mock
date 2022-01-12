af467078b8798a16a17448b9ef708f52bb3b988f
luorixo <luorixo@gmail.com>
2022-01-12 16:36:44 +1300

    changed favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
             <img src={image} alt="Botanic Garden Plants" className="absolute z-0 object-cover w-full h-full"/>
             <NavBar/>
             <section className="relative flex justify-center pt-0 lg:pt-32 md:pt-2 px-8">
-                <h1 className="text-6xl text-orange-100 font-bold cursive leading-none lg:leading-snug home-name">Hello!</h1>
+                <h1 className="text-6xl text-orange-100 font-bold cursive leading-none lg:leading-snug home-name">HELLO!</h1>
             </section>
         </main>
     );
