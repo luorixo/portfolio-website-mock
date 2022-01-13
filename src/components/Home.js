e35c91453496d4e9a2eafc0023d72d49ae1b7572
luorixo <luorixo@gmail.com>
2022-01-13 14:05:31 +1300

    tidied navbar styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,21 +4,31 @@ import NavBar from "./NavBar";
 export default function Home() {
     return (
         <main>
-            <hero className="min-w-full h-screen bg-gradient-to-bl from-orange-600 to-blue-700 block">
-                
-                <NavBar/>
-                <section className="relative flex justify-center pt-0 lg:pt-32 md:pt-2 px-8">
-                    <h1 className="text-6xl text-orange-100 font-bold cursive leading-none lg:leading-snug home-name">Hi there.</h1>
-                </section>
+            <hero className="min-w-full min-h-screen bg-gradient-to-bl from-orange-600 to-blue-700 block">
+                <NavBar/> 
+
+                <div className="flex">
+                    <section>
+                        <h1>Hi there.</h1>
+                    </section>
+                    <section>
+
+                    </section>
+                </div>
+
+
+                {/*<section className="relative flex justify-center pt-0 lg:pt-32 md:pt-2 px-8">
+                    <h1 className="text-6xl text-orange-300 font-bold cursive leading-none lg:leading-snug home-name">Hi there.</h1>
+                </section>*/}
             </hero>
 
-            <div class="custom-shape-divider-bottom-1641975661">
+            <div class="custom-shape-divider-bottom-1642025412">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
                 </svg>
             </div>
 
-            <section className="w-full h-screen bg-orange-300">
+            <section className="w-full h-screen blued">
                 <h1 className="relative">About Me</h1>
             </section>
         </main>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,17 +7,17 @@ export default function NavBar() {
     return (
         <header className="relative">
             <div className="container mx-auto flex justify-between">
-                <nav className="inline-flex">
+                <nav className="inline-flex py-8 my-6">
                     <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center mr-10 text-white text-4xl font-bold cursive tracking-widest">
                         EUGENE.C
                     </NavLink>
                     <NavLink to="/post" activeClassName="bg-red-700" className="inline-flex items-center px-8 text-white hover:text-orange-300"> 
                         BLOG
-                    </NavLink>
+                    </NavLink>  
                    
                 </nav>
-                <div className="inline-flex py-8 px-3 my-6">
-                    <SocialIcon url="https://www.linkedin.com/in/eugene-j-chua" label="linkedin link" className="mr-9" target="_blank" bgColor="#fff" style={{ height:55, width: 55}} />
+                <div className="inline-flex py-8 my-6">
+                    <SocialIcon url="https://www.linkedin.com/in/eugene-j-chua" label="linkedin link" className="mr-9 hover:text-black" target="_blank" bgColor="#fff" style={{ height:55, width: 55}} />
                     <SocialIcon url="https://github.com/luorixo" label="github link" className="mr-9" target="_blank" bgColor="#fff" style={{ height:55, width: 55}} />
                     <SocialIcon url="mailto:luorixo@gmail.com" label="mail link" target="_blank" bgColor="#fff" style={{ height:55, width: 55}} />
                 </div>
